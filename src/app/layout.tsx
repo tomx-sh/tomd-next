bd2de0a0fe6900153aa98e2ab23eadac095c6d7a
tomx <33961528+tomx-sh@users.noreply.github.com>
2026-08-30 12:06:34 +0200

    add mono font for badges

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
+import { IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
+});
+
+const ibmPlexMono = IBM_Plex_Mono({
+  weight: ["600"],
+  subsets: ["latin"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
@@ -17,7 +23,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", ibmPlexSans.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        ibmPlexSans.variable,
+        ibmPlexMono.variable,
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
